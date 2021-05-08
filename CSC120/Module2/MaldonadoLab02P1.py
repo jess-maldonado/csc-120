8764a967082621a26741570164792f8993ef96a2
jmaldonadorv <jess.maldonado11@gmail.com>
2021-05-08 16:42:05 -0400

    added files for the CSC 120 course

---
+++ CSC120/Module2/MaldonadoLab02P1.py
@@ -0,0 +1,7 @@
+x = int(input(" Please enter integer: "))
+if x < 0:
+    print("negative number")
+elif x % 2 == 0:
+    print("even number")
+else:
+    print("odd number")
---
+++ CSC120/Module2/MaldonadoLab02P2.py
@@ -0,0 +1,7 @@
+arr = []
+i = 0
+while len(arr) < 20:
+    if i % 2 == 0:
+        arr.append(i)
+        print(i)
+        i+=2
